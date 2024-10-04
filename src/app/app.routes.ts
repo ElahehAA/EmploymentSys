86ff29bf18919ef7be2eea1ab8512400c9456c16
ElahehAA <eaghabalazade1376@gmail.com>
2024-10-04 10:03:42 +0330

    first

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    // {
+    //     path:"Home",component:LayoutComponent,
+    //     pathMatch : 'prefix',
+    //     children:[
+    //         {path:"Test",component:TestComponent},
+    
+    
+    
+    
+    
+    //     ]
+    // },
+    // {path:"**",component:LayoutComponent}
+];

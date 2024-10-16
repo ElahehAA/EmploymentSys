ba7fda4757d453cc546f9e7e6468307684682789
ElahehAA <eaghabalazade@gnail.com>
2024-10-16 22:01:18 +0330

    inject HttpClient in standalone Component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { LoginDialogComponent } from './app/login-dialog/login-dialog.component';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  bootstrapApplication(LoginDialogComponent, {providers: [
+    provideHttpClient(),
+  ]});
